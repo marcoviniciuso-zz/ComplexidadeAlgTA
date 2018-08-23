fa5ef1fa1713a0439b6113313484c88eb63136af
Oliveira <marco.oliveira@sap.com>
2018-08-23 20:35:04 -0300

    Primeira versão

---
+++ index.js
@@ -0,0 +1,40 @@
+/*const http = require('http');
+const express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello World');
+});
+
+app.listen(8080);*/
+
+console.time('EXECUÇÃO');
+
+var numerosEncontrados = 0;
+var numero             = 0;
+while(numerosEncontrados < 15){
+    numero++;
+
+    var numeroAnterior    = numero - 1;
+    var somatorioAnterior = 0;
+    while(numeroAnterior != 0){
+        somatorioAnterior = somatorioAnterior + numeroAnterior;
+        numeroAnterior--;
+    }
+
+    var numeroPosterior    = numero;
+    var somatorioPosterior = 0;
+    
+    do{
+        numeroPosterior++;
+        somatorioPosterior = somatorioPosterior + numeroPosterior;
+    }while(somatorioPosterior < somatorioAnterior);
+
+    if(somatorioAnterior == somatorioPosterior){
+        console.log(numero + "     " + numeroPosterior);
+        numerosEncontrados++;
+    }
+}
+
+console.timeEnd('EXECUÇÃO');
